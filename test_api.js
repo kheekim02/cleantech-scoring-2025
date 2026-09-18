9325ef3abf70ff75fa5dbb5583aac0b575c19ca6
kheekim02 <kheekim@berkeley.edu>
2026-09-17 19:04:28 -0700

    chore: remove test scripts with hardcoded secrets

--- test_api.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://merry-toffee-84c633.netlify.app/.netlify/functions/list-startups?judge_id=J-001&passcode=demo2025', (resp) => {
-  let data = '';
-  resp.on('data', (chunk) => { data += chunk; });
-  resp.on('end', () => { console.log("Response:", data); });
-}).on("error", (err) => { console.log("Error:", err.message); });
--- test_local.js
+++
@@ -1,7 +0,0 @@
-const listStartups = require('./netlify/functions/list-startups').handler;
-
-async function run() {
-    const res = await listStartups({ queryStringParameters: { judge_id: 'J-001', passcode: 'demo2025' } });
-    console.log(res);
-}
-run();
